64073a2e1b0de968d7f7eab0d9316525547f1326
jbleduigou <jb.leduigou@gmail.com>
2021-09-21 18:35:37 +0200

    proper resources handling
    Signed-off-by: jbleduigou <jb.leduigou@gmail.com>

--- asdctool/src/main/java/org/openecomp/sdc/asdctool/main/DeleteComponentTool.java
+++ asdctool/src/main/java/org/openecomp/sdc/asdctool/main/DeleteComponentTool.java
@@ -37,24 +37,25 @@ public class DeleteComponentTool extends SdcInternalTool {
         disableConsole();
         ConsoleWriter.dataLine("STARTED... ");
         ConfigurationUploader.uploadConfigurationFiles(appConfigDir);
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(InternalToolConfiguration.class);
-        DeleteComponentHandler deleteComponentHandler = context.getBean(DeleteComponentHandler.class);
-        String input = "";
-        Scanner scanner = new Scanner(System.in);
-        do {
-            ConsoleWriter.dataLine("Enter next component unique id or exit: ");
-            input = scanner.nextLine();
-            if (!input.equals("exit")) {
-                if (!input.isEmpty()) {
-                    ConsoleWriter.dataLine("Your id is ", input);
-                    deleteComponentHandler.deleteComponent(input, scanner);
-                } else {
-                    ConsoleWriter.dataLine("Your id is empty. Try again.");
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(InternalToolConfiguration.class)) {
+            DeleteComponentHandler deleteComponentHandler = context.getBean(DeleteComponentHandler.class);
+            String input = "";
+            Scanner scanner = new Scanner(System.in);
+            do {
+                ConsoleWriter.dataLine("Enter next component unique id or exit: ");
+                input = scanner.nextLine();
+                if (!input.equals("exit")) {
+                    if (!input.isEmpty()) {
+                        ConsoleWriter.dataLine("Your id is ", input);
+                        deleteComponentHandler.deleteComponent(input, scanner);
+                    } else {
+                        ConsoleWriter.dataLine("Your id is empty. Try again.");
+                    }
                 }
-            }
-        } while (!input.equals("exit"));
-        deleteComponentHandler.closeAll();
-        ConsoleWriter.dataLine("DeleteComponentTool exit...");
-        System.exit(0);
+            } while (!input.equals("exit"));
+            deleteComponentHandler.closeAll();
+            ConsoleWriter.dataLine("DeleteComponentTool exit...");
+            System.exit(0);
+        }
     }
 }
